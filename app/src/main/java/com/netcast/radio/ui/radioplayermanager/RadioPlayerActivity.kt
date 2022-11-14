85c698b3b4ce502a711707de04b0add30eb1879a
Haroon <ateebhassan_vws@vizzwebsolutions.com>
2022-11-14 18:16:26 +0500

    Permission Issue Resolved

--- app/src/main/java/com/netcast/radio/ui/radioplayermanager/RadioPlayerActivity.kt
+++ app/src/main/java/com/netcast/radio/ui/radioplayermanager/RadioPlayerActivity.kt
@@ -1,6 +1,8 @@
 package com.netcast.radio.ui.radioplayermanager
 
 import android.Manifest
+import android.Manifest.permission.READ_EXTERNAL_STORAGE
+import android.Manifest.permission.WRITE_EXTERNAL_STORAGE
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.content.pm.PackageManager
@@ -13,12 +15,14 @@ import android.text.Html
 import android.util.Log
 import android.view.View
 import android.widget.Toast
-import androidx.annotation.RequiresApi
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.net.toUri
 import com.bumptech.glide.Glide
 import com.bumptech.glide.Priority
 import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
 import com.netcast.radio.BR
 import com.netcast.radio.PlayingChannelData
 import com.netcast.radio.R
@@ -30,8 +34,6 @@ import com.netcast.radio.download.DownloadFile
 import com.netcast.radio.request.AppConstants
 import com.netcast.radio.request.Resource
 import com.netcast.radio.ui.radioplayermanager.episodedata.Data
-import com.google.android.exoplayer2.ExoPlayer
-import com.google.android.exoplayer2.MediaItem
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -74,7 +76,11 @@ class RadioPlayerActivity() :
         exoPlayerManager("Normal")
         //
         uiControls()
-        requestPermission()
+//        requestPermission()
+        val result = checkPermission()
+        if (!result) {
+            requestPermission()
+        }
         CoroutineScope(Dispatchers.IO).launch {
             getOfflineData()
         }
@@ -260,16 +266,51 @@ class RadioPlayerActivity() :
         }
     }
 
-    private fun requestPermission() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+    private fun checkPermission(): Boolean {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            Environment.isExternalStorageManager()
+        } else {
+            val result =
+                ContextCompat.checkSelfPermission(this, READ_EXTERNAL_STORAGE)
+            val result1 =
+                ContextCompat.checkSelfPermission(this, WRITE_EXTERNAL_STORAGE)
+            result == PackageManager.PERMISSION_GRANTED && result1 == PackageManager.PERMISSION_GRANTED
+        }
+    }
 
+    private fun requestPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            try {
+                val intent = Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION)
+                intent.addCategory("android.intent.category.DEFAULT")
+                intent.data =
+                    Uri.parse(String.format("package:%s", applicationContext.packageName))
+                startActivityForResult(intent, 2296)
+            } catch (e: java.lang.Exception) {
+                val intent = Intent()
+                intent.action = Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION
+                startActivityForResult(intent, 2296)
+            }
+        } else {
+            //below android 11
             ActivityCompat.requestPermissions(
                 this,
-                permissions,
+                arrayOf(WRITE_EXTERNAL_STORAGE),
                 STORAGE_PERMISSION_REQUEST_CODE
             )
         }
     }
+
+    /*   private fun requestPermission() {
+           if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
+               ActivityCompat.requestPermissions(
+                   this,
+                   permissions,
+                   STORAGE_PERMISSION_REQUEST_CODE
+               )
+           }
+       }*/
 
     override fun onRequestPermissionsResult(
         requestCode: Int,
@@ -278,27 +319,39 @@ class RadioPlayerActivity() :
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         when (requestCode) {
-            STORAGE_PERMISSION_REQUEST_CODE -> if (grantResults.size > 0 && permissions[0] == Manifest.permission.WRITE_EXTERNAL_STORAGE) {
-                // check whether storage pe rmission granted or not.
-                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    if (if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-                            !Environment.isExternalStorageManager()
-                        } else {
-                            TODO("VERSION.SDK_INT < R")
-                        }
-                    ) {
-                        val intent = Intent()
-                        intent.action = Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION
-                        val uri: Uri = Uri.fromParts("package", this.packageName, null)
-                        intent.data = uri
-                        startActivity(intent)
-                    } else {
-
-                    }
+            STORAGE_PERMISSION_REQUEST_CODE -> if (grantResults.isNotEmpty()) {
+                val READ_EXTERNAL_STORAGE = grantResults[0] === PackageManager.PERMISSION_GRANTED
+                val WRITE_EXTERNAL_STORAGE = grantResults[1] === PackageManager.PERMISSION_GRANTED
+                if (READ_EXTERNAL_STORAGE && WRITE_EXTERNAL_STORAGE) {
+                    // perform action when allow permission success
+                } else {
+                    Toast.makeText(this, "Allow permission for storage access!", Toast.LENGTH_SHORT)
+                        .show()
                 }
             }
-            else -> {}
         }
+//        when (requestCode) {
+//            STORAGE_PERMISSION_REQUEST_CODE -> if (grantResults.size > 0 && permissions[0] == Manifest.permission.WRITE_EXTERNAL_STORAGE) {
+//                // check whether storage pe rmission granted or not.
+//                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                    if (if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+//                            !Environment.isExternalStorageManager()
+//                        } else {
+//                            TODO("VERSION.SDK_INT < R")
+//                        }
+//                    ) {
+//                        val intent = Intent()
+//                        intent.action = Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION
+//                        val uri: Uri = Uri.fromParts("package", this.packageName, null)
+//                        intent.data = uri
+//                        startActivity(intent)
+//                    } else {
+//
+//                    }
+//                }
+//            }
+//            else -> {}
+//        }
     }
 
     private fun createPlayingChannelData(it: Data) {
